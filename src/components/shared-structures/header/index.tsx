491557b22236c10883af5eaf1b37c0ceafc89e88
Rafał Izdebski <rafal987.1@o2.pl>
2023-12-29 17:28:39 +0100

    feature/5-create-main-page-for-mobile-view #5 (#6)
    * feat: create main page for mobile view
    
    * fix: change header backgroundcolor

--- src/components/shared-structures/header/index.tsx
+++ src/components/shared-structures/header/index.tsx
@@ -3,7 +3,7 @@ import { DesktopHeader } from './DesktopHeader';
 
 export const Header = () => {
   return (
-    <header className="px-4 w-screen h-[var(--header-height)] lg:px-0 lg:h-screen">
+    <header className="px-4 w-screen h-[var(--header-height)] lg:px-0 lg:h-screen bg-primary text-secondary">
       <MobileHeader />
       <DesktopHeader />
     </header>
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,15 @@
-import { Lato } from "next/font/google";
+import { Lato, Jura } from 'next/font/google';
 
 export const lato = Lato({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
-  style: ["italic", "normal"],
-  variable: "--font-lato",
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
+  style: ['italic', 'normal'],
+  variable: '--font-lato',
+});
+
+export const jura = Jura({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal'],
+  variable: '--font-jura',
 });
