042922421f0948a750ebe711634ca54de5e8422d
Rafixx <rafal987.1@o2.pl>
2023-12-31 19:43:44 +0100

    feat: expan main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
+
+// images
 import mainPhoto from '@/assets/images/main-photo.jpg';
+import firstPhoto from '@/assets/images/1.png';
+import secondPhoto from '@/assets/images/2.png';
+import thirdPhoto from '@/assets/images/3.png';
+import profilePhoto from '@/assets/images/mateusz-olifirowicz-profile.png';
+
+// icons
 import rightArrowIcon from '@/assets/svg/right-arrow.svg';
 import workerIcon from '@/assets/svg/worker.svg';
 import voltmeterIcon from '@/assets/svg/voltmeter.svg';
 import robotIcon from '@/assets/svg/robot.svg';
 import factoryIcon from '@/assets/svg/factory.svg';
 import powerPlantIcon from '@/assets/svg/power-plant.svg';
+import emailIcon from '@/assets/svg/email.svg';
+import phoneIcon from '@/assets/svg/phone.svg';
+import linkedInIcon from '@/assets/svg/linkedIn.svg';
 
 const advantagesOfCooperation = [
   'Solidnie i rzetelnie realizujemy każde zamówienie.',
@@ -43,6 +54,58 @@ const services = [
   },
 ];
 
+const realizations = [
+  {
+    headline: 'Sterownia przepompowni "Chiechanów"',
+    text: 'Inwestycja realizowana dla przepompowni “Ciechanów”. Zakres prac obejmował montaż oraz konfigurację głównego bloku sterownicznego.',
+    photo: firstPhoto,
+  },
+  {
+    headline: 'System SmartHome - Wola Chomejowa',
+    text: 'Projekt inteligentnego domu skupiający się na instalacji elektrycznej oraz montażu systemów oświetleniowych.',
+    photo: secondPhoto,
+  },
+  {
+    headline: 'Rozdzielnia z systemem automatyki',
+    text: 'Projektowanie oraz montaż systemu rozdzielnicy przemysłowej z elementami sterowniczymi centralnego układu elektrycznego.',
+    photo: thirdPhoto,
+  },
+];
+
+const data = {
+  companyInfo: {
+    companyName: 'Olmat Mateusz Olifirowicz',
+    nip: '8252139859',
+    regon: '386857603',
+    adres: {
+      city: 'Warszawa',
+      zipCode: '03-982',
+      street: 'Jana Nowaka-Jeziorańskiego',
+      building: '44',
+      apartment: '18',
+      icon: '',
+    },
+  },
+  profilePhoto: profilePhoto,
+  contactInfo: [
+    {
+      description: 'mateuszolifirowicz@gmail.com',
+      link: 'mateuszolifirowicz@gmail.com',
+      icon: emailIcon,
+    },
+    {
+      description: '517-788-363',
+      link: '517788363',
+      icon: phoneIcon,
+    },
+    {
+      description: 'LinkedIn',
+      link: '#',
+      icon: linkedInIcon,
+    },
+  ],
+};
+
 export default function Home() {
   return (
     // lg:flex-row
@@ -51,6 +114,7 @@ export default function Home() {
       <AboutSection />
       <ServicesSection />
       <RealizationsSection />
+      <ContactSection />
     </>
   );
 }
@@ -71,8 +135,8 @@ const LandingSection = () => {
               </h2>
             </div>
             <div className="mt-4 lg:mt-12">
-              <button className="bg-[#FFDE8A] p-4 rounded-md lg:w-[400px]">
-                <span className="font-lato font-bold text-sm text-primary">
+              <button className="bg-primary p-4 rounded-md lg:w-[400px]">
+                <span className="font-lato font-bold text-sm text-secondary">
                   Sprawdź dotychczasowe realizacje
                 </span>
               </button>
@@ -184,17 +248,84 @@ const RealizationsSection = () => {
           <h4 className="text-xl font-bold leading-6">
             Jakie inwestycje ukończyliśmy w ostatnim czasie?
           </h4>
-          {/* <ul className="grid grid-cols-2 gap-4">
-          {services.map((service, _) => (
-            <li
-              className="flex flex-col gap-3 items-center p-4 min-h-32 h-full bg-secondary bg-opacity-75 rounded-md"
-              key={_}
-            >
-              <Image src={resistanceIcon} width={40} alt="" aria-hidden />
-              <span className="text-center">{service}</span>
+          <ul className="grid grid-cols-1 gap-6 -ml-4 w-screen">
+            {realizations.map((realization, _) => (
+              <li
+                className="flex flex-col gap-6 items-center px-4 py-8 min-h-32 h-full bg-secondary bg-opacity-75 rounded-md"
+                key={_}
+              >
+                <h5 className="text-xl text-center font-bold">
+                  {realization.headline}
+                </h5>
+                <Image
+                  className="ml-0 w-full rounded-lg"
+                  src={realization.photo}
+                  alt={realization.headline}
+                  aria-description={realization.headline}
+                />
+                <span className="">{realization.text}</span>
+                <button className="bg-primary px-4 py-3 rounded-md text-secondary">
+                  Czytaj więcej
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export const ContactSection = () => {
+  return (
+    <section className="p-4">
+      <div className="space-y-8">
+        <SectionHeadline headline="Kontakt" />
+        <div className="space-y-1">
+          <p>W przypadku pytań zachęcamy do kontaktu.</p>
+        </div>
+        <Image src={data.profilePhoto} alt="Mateusz Olifirowicz Olmat" />
+        <div className="space-y-8">
+          <ul className="space-y-2">
+            {data.contactInfo.map((contact, _) => (
+              <li key={_}>
+                <Link href={contact.link} className="flex gap-4">
+                  <Image src={contact.icon} alt="" aria-hidden />
+                  <span>{contact.description}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* <h4 className="text-xl font-bold leading-6">
+            Jakie inwestycje ukończyliśmy w ostatnim czasie?
+          </h4> */}
+          <ul className="space-y-1">
+            <li>
+              <span>{data.companyInfo.companyName}</span>
             </li>
-          ))}
-        </ul> */}
+            <li>
+              <span>Nip: {data.companyInfo.nip}</span>
+            </li>
+            <li>
+              <span>Regon: {data.companyInfo.regon}</span>
+            </li>
+          </ul>
+          <ul className="space-y-1">
+            <li>
+              <span>ul. {data.companyInfo.adres.street}</span>
+            </li>
+            <li>
+              <span>
+                bud. {data.companyInfo.adres.building}/
+                {data.companyInfo.adres.apartment}
+              </span>
+            </li>
+            <li>
+              <span>
+                {data.companyInfo.adres.zipCode} {data.companyInfo.adres.city}
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
