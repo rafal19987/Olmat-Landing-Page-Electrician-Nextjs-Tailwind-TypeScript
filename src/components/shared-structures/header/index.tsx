a83781ca9149193929f7aabcd5039d405e942d11
Rafał Izdebski <rafal987.1@o2.pl>
2024-01-08 08:05:57 +0100

    refactor/15-recreate-desktop-header #15 (#16)
    * feat: add navigation links to websiteConfig file
    
    * refactor: get navigation links to mobilemenu component from config file
    
    * feat: add company logo
    
    * fix: change header height on desktop
    
    * refactor: recreate desktop header

--- src/components/shared-structures/header/index.tsx
+++ src/components/shared-structures/header/index.tsx
@@ -3,7 +3,7 @@ import { DesktopHeader } from './DesktopHeader';
 
 export const Header = () => {
   return (
-    <header className="px-4 w-screen h-[var(--header-height)] lg:px-0 lg:h-screen bg-primary text-secondary lg:bg-secondary">
+    <header className="px-4 w-screen h-[var(--header-height)] lg:h-[var(--header-height-desktop)] bg-primary text-secondary ">
       <MobileHeader />
       <DesktopHeader />
     </header>
