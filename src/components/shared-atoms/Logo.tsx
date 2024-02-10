d65b9e73d7c1d124fefd5e9de20d363279d1d516
Rafixx <rafal987.1@o2.pl>
2024-02-10 11:39:34 +0100

    fix: wrap logo component into Link

--- src/components/shared-atoms/Logo.tsx
+++ src/components/shared-atoms/Logo.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
 import logoCompany from '@/assets/svg/logo-company.svg';
+import Link from 'next/link';
 
 export const Logo: React.FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <Image
-      src={logoCompany}
-      alt="Logo firmy Olmat"
-      aria-description="Logo firmy Olmat"
-    />
+    <Link href="/">
+      <Image
+        src={logoCompany}
+        alt="Logo firmy Olmat"
+        aria-description="Logo firmy Olmat"
+        priority
+      />
+    </Link>
   );
 };
